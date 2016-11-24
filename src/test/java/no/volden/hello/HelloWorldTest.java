10e81abee88db856cdd9dafa6ff8a9d8f9adc2a6
ov <oyvinvo@gmail.com>
2016-11-24 09:35:58 +0100

    Added display and tag annotations

--- src/test/java/no/volden/hello/HelloWorldTest.java
+++ src/test/java/no/volden/hello/HelloWorldTest.java
@@ -2,9 +2,7 @@ package no.volden.hello;
 
 import no.volden.Main;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -13,31 +11,35 @@ import javax.ws.rs.core.MediaType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@DisplayName("HelloWorld services should greet in both text and json")
 class HelloWorldTest {
 
 	private static HttpServer server;
 	private static WebTarget target;
 
 	@BeforeAll
-	public static void setUp() {
+	public static void initAll() {
 		server = Main.startServer();
 		Client c = ClientBuilder.newClient();
 		target = c.target(Main.BASE_URI);
 	}
 
 	@AfterAll
-	static void tearDown() {
+	static void tearDownAll() {
 		server.shutdownNow();
 	}
 
 	@Test
+	@DisplayName("I want to be greeted in plain text")
+	@Tag("text_respons")
 	void hiya() {
 		String response = target.path("HelloWorld").request(MediaType.TEXT_PLAIN).get(String.class);
 		assertEquals("Hi world!", response);
 	}
 
 	@Test
+	@DisplayName("I want to be greeted in json")
+	@Tag("json_respons")
 	void hiyaJason() {
 		HiyaJson response = target.path("HelloWorld").request(MediaType.APPLICATION_JSON).get(HiyaJson.class);
 		assertEquals("Hi world!", response.getLabel());
